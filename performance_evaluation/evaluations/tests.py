aa995c0aa67a024e6a45d21c8c68d2ae8438e669
Walison Filipe <walisonfilipe@hotmail.com>
2019-10-25 22:54:30 -0300

    Add simple test

--- performance_evaluation/evaluations/tests.py
+++ performance_evaluation/evaluations/tests.py
@@ -1,3 +1,15 @@
 from django.test import TestCase
+from model_bakery import baker
 
-# Create your tests here.
+from .models import Evaluation, EvaluationSkill
+
+
+class TestEvalutaionModel(TestCase):
+    def setUp(self):
+        self.evaluation = baker.make(Evaluation)
+
+    def test_average(self):
+        baker.make(EvaluationSkill, evaluation=self.evaluation, grade=2),
+        baker.make(EvaluationSkill, evaluation=self.evaluation, grade=4),
+
+        self.assertEqual(self.evaluation.average(), 3)
