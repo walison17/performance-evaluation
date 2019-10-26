92ebe775392f1c26a6a2ed25b595ac92f57861c8
Walison Filipe <walisonfilipe@hotmail.com>
2019-10-25 23:22:35 -0300

    Add test

--- performance_evaluation/evaluations/tests.py
+++ performance_evaluation/evaluations/tests.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 from django.test import TestCase
 from model_bakery import baker
 
@@ -9,7 +11,11 @@ class TestEvalutaionModel(TestCase):
         self.evaluation = baker.make(Evaluation)
 
     def test_average(self):
-        baker.make(EvaluationSkill, evaluation=self.evaluation, grade=2),
-        baker.make(EvaluationSkill, evaluation=self.evaluation, grade=4),
+        baker.make(EvaluationSkill, evaluation=self.evaluation, grade=2)
+        baker.make(EvaluationSkill, evaluation=self.evaluation, grade=4)
 
         self.assertEqual(self.evaluation.average(), 3)
+
+    def test_default_year_is_current_year(self):
+        now = datetime.now()
+        self.assertEqual(now.year, self.evaluation.year)
